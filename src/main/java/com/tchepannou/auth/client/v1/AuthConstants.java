b9e7e1188304efc80ab5caefd7b5e66ff2381920
htchepannou <htchepannou@expedia.com>
2015-09-07 21:40:17 -0400

    remove metrics

--- src/main/java/com/tchepannou/auth/client/v1/AuthConstants.java
+++ src/main/java/com/tchepannou/auth/client/v1/AuthConstants.java
@@ -7,10 +7,6 @@ public class AuthConstants {
     public static final String ERROR_TOKEN_NOT_FOUND = "token_not_found";
     public static final String ERROR_BAD_TOKEN = "bad_token";
 
-    public static final String METRIC_LOGIN = "auth-login";
-    public static final String METRIC_LOGOUT = "auth-logout";
-    public static final String METRIC_ACCESS_TOKEN = "auth-access-token";
-
     public static final String QUEUE_EVENT_LOG = "auth-queue-event-log";
     public static final String EVENT_LOGIN = "auth-login";
     public static final String EVENT_LOGOUT = "auth-logout";
